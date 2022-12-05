316dba759a1e27b0fbf46e52354f79fbee5eb69f
Sachin.g.s <gssachin1997@gmail.com>
2022-12-05 16:54:40 +0530

    message

--- src/test/java/Samplepaject/DynomicTest.java
+++ src/test/java/Samplepaject/DynomicTest.java
@@ -1,6 +1,9 @@
 package Samplepaject;
 
+import org.testng.annotations.Test;
+
 public class DynomicTest{
+	@Test
 	public void DynomicTest() {
 		System.out.println("hello");
 	}
--- src/test/java/Samplepaject/PrintaddnoTest.java
+++ src/test/java/Samplepaject/PrintaddnoTest.java
@@ -1,5 +1,11 @@
 package Samplepaject;
 
+import org.testng.annotations.Test;
+
 public class PrintaddnoTest {
+	@Test
+	public void PrintaddnoTest() {
+		System.out.println("hii");
+	}
 
 }
